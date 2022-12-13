e1addc78cb0f6433f98b612fb87a85dd38a099ee
Joe Ng <joengsh@gmail.com>
2022-12-13 06:07:25 +0000

    update app

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './app.css';
 import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
 import LoginSection from './components/LoginSection/LoginSection';
 import Navbar from './components/Navbar/Navbar';
 
@@ -8,22 +9,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <div
-        className="hero min-h-screen"
-        style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-              exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+      <Hero />
       <LoginSection />
       <Footer />
     </div>

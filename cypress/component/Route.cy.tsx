601b2cab5f4cdc9000ff51d074a7de3cbefc8ec0
Joe Ng <joengsh@gmail.com>
2023-01-12 20:50:25 +0000

    test: update Route cypress test

--- cypress/component/Route.cy.tsx
+++ cypress/component/Route.cy.tsx
@@ -3,15 +3,33 @@ import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 
 const router = createMemoryRouter(routes);
 
-describe('Route', () => {
-  it('playground', () => {
+describe('Given the main application router is shown', () => {
+  beforeEach(() => {
     cy.viewport(1280, 720);
     cy.mount(<RouterProvider router={router} />);
-    cy.get('[data-testid="nav-home"]').click();
-    cy.get('[data-testid="app"]').should('be.visible');
-    cy.get('[data-testid="nav-page"]').click();
-    cy.get('[data-testid="page"]').should('be.visible');
-    cy.get('[data-testid="nav-about"]').click();
-    cy.get('[data-testid="about"]').should('be.visible');
+  });
+  context('When I click on home tab', () => {
+    beforeEach(() => {
+      cy.get('[data-testid="nav-home"]').click();
+    });
+    it('Then I should see the app page', () => {
+      cy.get('[data-testid="app"]').should('be.visible');
+    });
+  });
+  context('When I click on page tab', () => {
+    beforeEach(() => {
+      cy.get('[data-testid="nav-page"]').click();
+    });
+    it('Then I should see the page page', () => {
+      cy.get('[data-testid="page"]').should('be.visible');
+    });
+  });
+  context('When I click on about tab', () => {
+    beforeEach(() => {
+      cy.get('[data-testid="nav-about"]').click();
+    });
+    it('Then I should see the about page', () => {
+      cy.get('[data-testid="about"]').should('be.visible');
+    });
   });
 });

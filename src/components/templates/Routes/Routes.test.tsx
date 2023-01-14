e2b6f7b9c6cc4515eb17cb62cb7ab21ed9dbef95
Joe Ng <joengsh@gmail.com>
2023-01-14 14:00:03 +0000

    refactor: replace all yarn in script into npm run, refactor Routes.test.tsx

--- src/components/templates/Routes/Routes.test.tsx
+++ src/components/templates/Routes/Routes.test.tsx
@@ -23,30 +23,11 @@ describe('Route', () => {
     render(<RouterProvider router={router} />);
     // TODO: click app go app, click page go page, click about go about
     await waitFor(() => expect(screen.getByTestId('nav-home')).toBeInTheDocument());
-    fireEvent(
-      screen.getByTestId('nav-home'),
-      new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-      })
-    );
+    fireEvent.click(screen.getByTestId('nav-home'));
     await waitFor(() => expect(screen.getByTestId('app')).toBeInTheDocument());
-
-    fireEvent(
-      screen.getByTestId('nav-page'),
-      new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-      })
-    );
+    fireEvent.click(screen.getByTestId('nav-page'));
     await waitFor(() => expect(screen.getByTestId('page')).toBeInTheDocument());
-    fireEvent(
-      screen.getByTestId('nav-about'),
-      new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-      })
-    );
+    fireEvent.click(screen.getByTestId('nav-about'));
     await waitFor(() => expect(screen.getByTestId('about')).toBeInTheDocument());
   });
 });

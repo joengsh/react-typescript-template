73e057f53859a76b48dea94010005ee140fe5b1d
Joe Ng <joengsh@gmail.com>
2023-01-18 13:46:17 +0000

    test: integrate storybook Play with jest

--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { Page } from './Page';
 
 export default {
@@ -23,4 +24,5 @@ LoggedIn.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
   const loginButton = await canvas.getByRole('button', { name: /Log in/i });
   await userEvent.click(loginButton);
+  expect(canvas.getByText(/Log out/i)).toBeInTheDocument();
 };
